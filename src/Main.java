c33eaf85f9702366282a96d47932d708ddb1534a
levje1932644 <1932644@etu.cegepjonquiere.ca>
2021-02-16 11:39:20 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import vue.Menu;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        BorderPane bdp = new BorderPane();
+        Menu.menuPrincipale(bdp);
+        Scene scene = new Scene(bdp, 800, 600);
+
         primaryStage.setTitle("Putt-it Golf");
-        primaryStage.setScene(new Scene(null, 300, 275));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
