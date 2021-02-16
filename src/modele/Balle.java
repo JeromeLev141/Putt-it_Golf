9c5a349fcf13270706d18fc5268b9f33b3d03b5c
levje1932644 <1932644@etu.cegepjonquiere.ca>
2021-02-16 10:15:29 -0500

    balle et sol

--- src/modele/Balle.java
+++ src/modele/Balle.java
@@ -4,6 +4,13 @@ import javafx.geometry.Point3D;
 
 public class Balle {
 
-    Point3D position;
-    
+    private Point3D position;
+
+    Balle (Point3D position) {
+        this.position = position;
+    }
+
+    public Point3D getPosition() { return position; }
+
+    public void setPosition(Point3D position) { this.position = position; }
 }
