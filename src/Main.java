600c803e7e4594eacbc351de46cd5ec56fb6c72a
levje1932644 <1932644@etu.cegepjonquiere.ca>
2021-02-16 09:40:57 -0500

    structure

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-package sample;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +6,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Putt-it Golf");
+        primaryStage.setScene(new Scene(null, 300, 275));
         primaryStage.show();
     }
 
