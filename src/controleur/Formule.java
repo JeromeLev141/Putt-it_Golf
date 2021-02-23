d69285346765468dc4cca75dd85b29a0f29cb2be
levje1932644 <1932644@etu.cegepjonquiere.ca>
2021-02-23 08:35:20 -0500

    MRUA

---
+++ src/controleur/Formule.java
@@ -0,0 +1,10 @@
+package controleur;
+
+public class Formule {
+
+    public static double MRUA(double position, double vitesse, double acceleration, double temps) {
+        return position + (vitesse * temps + 0.5 * acceleration * Math.pow(temps, 2));
+    }
+
+    
+}
---
+++ src/modele/Plateforme.java
@@ -0,0 +1,14 @@
+package modele;
+
+public class Plateforme {
+
+    public static String getNiveau1() {
+        return new String("xooox\n" +
+                "xooox\n" +
+                "xoooxxxxxxx\n" +
+                "xooooooooox\n" +
+                "xooooooooox\n" +
+                "xooooooooox\n" +
+                "xxxxxxxxxxx");
+    }
+}
