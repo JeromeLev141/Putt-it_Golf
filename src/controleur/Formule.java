e873385dfdf2a66fef262bad429e2d4623a070f9
levje1932644 <1932644@etu.cegepjonquiere.ca>
2021-02-23 08:40:43 -0500

    rebondissement

--- src/controleur/Formule.java
+++ src/controleur/Formule.java
@@ -2,9 +2,11 @@ package controleur;
 
 public class Formule {
 
-    public static double MRUA(double position, double vitesse, double acceleration, double temps) {
-        return position + (vitesse * temps + 0.5 * acceleration * Math.pow(temps, 2));
+    public static double MRUA(double position, double vitesse, double frottement, double temps) {
+        return position + (vitesse * temps + 0.5 * frottement * Math.pow(temps, 2));
     }
 
-    
+    public static double rebondissement(Vecteur balle, Vecteur structure) {
+        return 0;
+    }
 }
