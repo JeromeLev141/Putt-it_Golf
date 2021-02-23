dcb042bfa861356363df2944b3d93c6ae2b21e38
levje1932644 <1932644@etu.cegepjonquiere.ca>
2021-02-23 11:49:29 -0500

    sols

--- src/modele/Plateforme.java
+++ src/modele/Plateforme.java
@@ -2,6 +2,11 @@ package modele;
 
 public class Plateforme {
 
+    private final Sol eau = new Sol(true, 0);
+    private final Sol beton = new Sol(false, 0.9);
+    private final Sol bois = new Sol(false, 0.5);
+    private final Sol gazon = new Sol(false, 2);
+
     public static String getNiveau1() {
         return new String("xooox\n" +
                 "xooox\n" +
