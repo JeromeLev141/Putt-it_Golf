3dcbb0f3e5c29a81ce918d5e32ceee2271e08f12
levje1932644 <1932644@etu.cegepjonquiere.ca>
2021-03-18 19:08:47 -0400

    scoreboard

--- src/ScoreChecker.java
+++ src/ScoreChecker.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ScoreChecker {
 
@@ -21,24 +22,19 @@ public class ScoreChecker {
         try {
             String entree = new String(Files.readAllBytes(file.toPath()));
             entree = entree.replace("\n", ",").replace("\r", "");
-            entree = entree.replaceAll("\\s", "");
 
-            String[] scores = entree.split(",");
-            HashMap<Integer, String> map = new HashMap<>();
-            for (int x = 0; x < (scores.length / 2); x++) {
-                map.put(Integer.parseInt(scores[x * 2]), scores[1 + 2 * x]);
-            }
-
-            List<Integer> scoresTries = new ArrayList<>(map.keySet());
-            Collections.sort(scoresTries);
-            Collections.reverse(scoresTries);
+            String[] token = entree.split(",");
+            List<String> scores = new ArrayList<>(Arrays.asList(token));
+            scores = scores.stream().sorted().collect(Collectors.toList());
+            Collections.reverse(scores);
 
             String[] top5 = new String[5];
             for (int i = 0; i < 5; i++)
-                if (i < scoresTries.size())
-                    top5[i] = scoresTries.get(i) + " - " + map.get(scoresTries.get(i));
+                if (i < scores.size())
+                    top5[i] = scores.get(i);
                 else
                     top5[i] = "0 - vide";
+                
             System.out.println(Arrays.toString(top5));
 
         } catch (IOException e) {
