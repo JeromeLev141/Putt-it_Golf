e2efeb7c3c596b23edca6b2fe143da39ea694985
jeremi_girard <girje1932987@etu.cegepjonquiere.ca>
2021-04-08 13:41:23 -0400

    insersion formule 3d

--- src/Main_jeremi.java
+++ src/Main_jeremi.java
@@ -8,25 +8,15 @@ import java.util.Vector;
 public class Main_jeremi{
     public static void main(String[]args){
         Vecteur vecteur = new Vecteur(new Point3D(0,0,0));
-        vecteur.addforceX(15.0);
+        vecteur.creeSection();
+        vecteur.setForceX(0,10.0);
         Sol gazon = new Sol(4);
 
-        System.out.println(gazon.getFrottement());
-        int position1 = vecteur.addforceX(-5);
-        System.out.println(vecteur.getForceX());
-        vecteur.addforceX(-7);
-        vecteur.addforceX(6);
-        vecteur.addforceY(10);
-        System.out.println(vecteur.getAngleXZ());
-        vecteur.addVitesseX(5);
-        System.out.println(vecteur.getAngleXZ());
-        vecteur.addVitesseZ(-5);
-        System.out.println(vecteur.getVecteurVitesseResultant()[0] + "-" + vecteur.getVecteurVitesseResultant()[1] + "-" + vecteur.getVecteurVitesseResultant()[2]);
-        System.out.println(vecteur.getAngleXZ());
-        System.out.println(vecteur.getVecteurForceResultant()[0] + "-" + vecteur.getVecteurForceResultant()[1] + "-" + vecteur.getVecteurForceResultant()[2]);
-        vecteur.setForceX(position1,-2);
-        System.out.println(vecteur.getForceX());
-        System.out.println(vecteur.getVecteurForceResultant()[0] + "-" + vecteur.getVecteurForceResultant()[1] + "-" + vecteur.getVecteurForceResultant()[2]);
+        System.out.println( "Sol = " + gazon.getFrottement());
+        vecteur.setForceY(0,0);
+        vecteur.setForceZ(0,10);
+        vecteur.refreshVecteurAccelerationResultant();
+        System.out.println(vecteur.getVecteurForceResultant()[0] + " : " + vecteur.getVecteurForceResultant()[1] + " : " + vecteur.getVecteurForceResultant()[2]);
 
         FormeCordonneSommet forme = new FormeCordonneSommet(new Point3D(5,-0.5,10),10,1,20, 15,41,4);
         List<FormeCordonneSommet> liste = new ArrayList<>();
