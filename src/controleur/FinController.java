aa605adb8846c6c8cc987c8d36d375e5d98d9685
levje1932644 <1932644@etu.cegepjonquiere.ca>
2021-04-15 17:23:37 -0400

    deroulement des parties

---
+++ src/controleur/FinController.java
@@ -0,0 +1,61 @@
+package controleur;
+
+import controleur.Jeux;
+import controleur.ScoreChecker;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class FinController {
+
+    @FXML
+    private VBox vBox;
+    @FXML
+    private Label node1;
+    @FXML
+    private TextField node2;
+
+    private Jeux jeux;
+    private int score;
+
+    public void transfert(Jeux jeux, int score) {
+        this.jeux = jeux;
+        this.score = score;
+
+        node1.setText(node1.getText() + score);
+        node2.textProperty().addListener((ov, oldValue, newValue) -> {
+            if (node2.getText().length() > 8) {
+                String s = node2.getText().substring(0, 8);
+                node2.setText(s);
+            }
+        });
+    }
+
+    @FXML
+    private void retour() throws IOException {
+        String nb = String.valueOf(score);
+        if (nb.length() == 2)
+            nb = "0" + nb;
+
+        ScoreChecker.ecrire(nb + " - " + node2.getText());
+        jeux.sonRetour();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("SceneMenu.fxml"));
+        Parent option = loader.load();
+
+        MenuController menuController = loader.getController();
+        menuController.transfert(jeux);
+
+        Scene scene = new Scene(option, 800, 600);
+        Stage stage = (Stage) node1.getScene().getWindow();
+        stage.setScene(scene);
+    }
+}
